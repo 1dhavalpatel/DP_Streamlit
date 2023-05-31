6c46dd3d100b0dd9efb7ff426c0dad655dd76408
1dhavalpatel <38314040+1dhavalpatel@users.noreply.github.com>
2023-05-31 12:38:19 +0530

    Update Fin_Performance.py

--- Fin_Performance.py
+++ Fin_Performance.py
@@ -1,13 +1,18 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
 
-st.title("Hi Anber! Welcome to Streamlit!")
+st.title("World! Welcome to Streamlit!")
 
-st.write("My first DataFrame")
+fig = plt.figure(figsize=(16,9), dpi=400)
+plt.bar(dfFinNumbers.Month, dfFinNumbers.GrossExpense, color= 'orange', label="Gross Expense")
+plt.bar(dfFinNumbers.Month, dfFinNumbers.GrossIncome, color= 'green', label="Gross Income")
+plt.plot(dfFinNumbers.Month, dfFinNumbers.NetIncome, color= 'blue', label="Net Income", marker='*')
 
-st.write(
-pd.DataFrame({
-    'A': [1, 5, 9, 7],
-    'B': [3, 2, 4, 8]
-  })
-)
+plt.title('KHH Numbers in Lakh', y=1.05, fontsize=18, color='blue')
+
+# calling the function to add value labels
+#addlabels(dfFinNumbers.RptMonth, dfFinNumbers.NetIncome)
+
+plt.legend()
+st.pyplot(fig)
