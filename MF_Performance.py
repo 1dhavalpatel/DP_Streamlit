1e9964d273ef77904ef454be8612996893bd2a36
1dhavalpatel <38314040+1dhavalpatel@users.noreply.github.com>
2023-11-18 13:00:16 +0530

    base df

--- MF_Performance.py
+++ MF_Performance.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import streamlit as st
 
-st.title("Welcome to DP's Mutual funds Performance Tracker!")
+### Data Import ###
+dfMF = pd.read_csv("./Mutual funds Performance Tracker Nov 23.csv")
 
-dfMF = pd.read_csv('Mutual funds Performance Tracker Nov 23.csv')
+### INTRODUCTION ###
+st.set_page_config(layout="wide")
+st.title("Welcome to DP's Mutual funds Performance Tracker!")
+st.subheader('Data as of 16-Nov-2023')
+
+### Sidebar SELECTION ###
+oneY = st.slider('Select returns for 1Y', 0, 100, 10)
+threeY = st.slider('Select returns for 3Y', 0, 100, 10)
+
+### Helper Methods ###
+
+### Main Page content ###
+st.dataframe(dfMF) # Same as st.write(dfMF)
