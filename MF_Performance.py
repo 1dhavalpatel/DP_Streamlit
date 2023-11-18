c5e76450bd5dc94b0dc1046c4641af953357b81a
1dhavalpatel <38314040+1dhavalpatel@users.noreply.github.com>
2023-11-18 17:45:00 +0530

    Update MF_Performance.py

--- MF_Performance.py
+++ MF_Performance.py
@@ -46,15 +46,15 @@ dfMF = dfMF[dfMF['10Y'] >= tenY]
 col1, col2, col3 = st.columns(3)
 
 with col1:
-   st.header("Average AuM(Cr)")
+   st.subheader("Average AuM(Cr)")
    st.write(dfMF['AuM (Cr)'].mean().round(2))
 
 with col2:
-   st.header("Average 1Y return")
+   st.subheader("Average 1Y return")
    st.write(dfMF['1Y'].mean().round(2))
 
 with col3:
-   st.header("Average 5Y return")
+   st.subheader("Average 5Y return")
    st.write(dfMF['5Y'].mean().round(2))
 
-st.dataframe(dfMF[['Scheme Name','Crisil Rank','AuM (Cr)','3M','6M','1Y','2Y','3Y','5Y','10Y']]) # Same as st.write(dfMF)
+st.dataframe(dfMF[['Scheme Name','Crisil Rank','AuM (Cr)','3M','6M','1Y','2Y','3Y','5Y','10Y']], hide_index=True) # Same as st.write(dfMF)
