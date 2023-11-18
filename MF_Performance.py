5b6c9e296f87f7d6367bcf90c1dc41f4f9bf7409
1dhavalpatel <38314040+1dhavalpatel@users.noreply.github.com>
2023-11-18 16:49:11 +0530

    base filter

--- MF_Performance.py
+++ MF_Performance.py
@@ -5,12 +5,14 @@ import streamlit as st
 dfMF = pd.read_csv("./Mutual funds Performance Tracker Nov 23.csv")
 
 ### INTRODUCTION ###
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 st.title("Welcome to DP's Mutual funds Performance Tracker!")
 st.subheader('Data as of 16-Nov-2023')
 
 ### Sidebar SELECTION ###
 oneY = st.slider('Select returns for 1Y', 0, 100, 10)
+dfMF = dfMF[dfMF['1Y'] > oneY]
+
 threeY = st.slider('Select returns for 3Y', 0, 100, 10)
 
 ### Helper Methods ###
