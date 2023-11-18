63fb03d7f16c5428c8e0ca68b5ae30738b67939a
1dhavalpatel <38314040+1dhavalpatel@users.noreply.github.com>
2023-11-18 17:09:21 +0530

    adding more filters

--- MF_Performance.py
+++ MF_Performance.py
@@ -4,6 +4,17 @@ import streamlit as st
 ### Data Import ###
 dfMF = pd.read_csv("./Mutual funds Performance Tracker Nov 23.csv")
 
+### Data Cleanup ###
+dfMF['1W'] = dfMF['1W'].str.replace('%', '').fillna(0).astype(float)
+dfMF['1M'] = dfMF['1M'].str.replace('%', '').fillna(0).astype(float)
+dfMF['3M'] = dfMF['3M'].str.replace('%', '').fillna(0).astype(float)
+dfMF['6M'] = dfMF['6M'].str.replace('%', '').fillna(0).astype(float)
+dfMF['1Y'] = dfMF['1Y'].str.replace('%', '').fillna(0).astype(float)
+dfMF['2Y'] = dfMF['2Y'].str.replace('%', '').fillna(0).astype(float)
+dfMF['3Y'] = dfMF['3Y'].str.replace('%', '').fillna(0).astype(float)
+dfMF['5Y'] = dfMF['5Y'].str.replace('%', '').fillna(0).astype(float)
+dfMF['10Y'] = dfMF['10Y'].str.replace('%', '').fillna(0).astype(float)
+
 ### INTRODUCTION ###
 #st.set_page_config(layout="wide")
 st.title("Welcome to DP's Mutual funds Performance Tracker!")
@@ -11,9 +22,16 @@ st.subheader('Data as of 16-Nov-2023')
 
 ### Sidebar SELECTION ###
 oneY = st.slider('Select returns for 1Y', 0, 100, 10)
-dfMF = dfMF[dfMF['1Y'] > oneY]
+dfMF = dfMF[dfMF['1Y'] >= oneY]
 
 threeY = st.slider('Select returns for 3Y', 0, 100, 10)
+dfMF = dfMF[dfMF['3Y'] >= threeY]
+
+fiveY = st.slider('Select returns for 5Y', 0, 100, 10)
+dfMF = dfMF[dfMF['5Y'] >= fiveY]
+
+tenY = st.slider('Select returns for 10Y', 0, 100, 10)
+dfMF = dfMF[dfMF['10Y'] >= tenY]
 
 ### Helper Methods ###
 
